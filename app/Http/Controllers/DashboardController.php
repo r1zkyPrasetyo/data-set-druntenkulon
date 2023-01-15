8db1aefb73b32c6b95e86d3ce0d3f376c49358c9
rsvn.id <emailrizkyprasetyo@gmail.com>
2023-01-15 20:34:59 +0700

    add

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -139,7 +139,7 @@ class DashboardController extends Controller
                 $join->on('tweb_desa_pamong.id_pend', '=', 'tweb_penduduk.id');
             })
             ->where('tweb_desa_pamong.jabatan_id','1')
-            ->select('tweb_penduduk.nama','tweb_penduduk.foto','ref_jabatan.nama as jabatan','tweb_desa_pamong.pamong_masajab','tweb_desa_pamong.gelar_belakang')
+            ->select('tweb_penduduk.nama','tweb_desa_pamong.foto','ref_jabatan.nama as jabatan','tweb_desa_pamong.pamong_masajab','tweb_desa_pamong.gelar_belakang')
             ->first();
             return $data;
         } catch (\Exception $e) {
